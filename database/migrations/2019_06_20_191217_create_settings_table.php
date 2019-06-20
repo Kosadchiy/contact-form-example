4c5f018c75c57be57c44e44a7467b5d62a955bcb
KOsadchiy <kosadchij@inbox.ru>
2019-06-21 01:51:38 +0300

    fix settings table

--- database/migrations/2019_06_20_191217_create_settings_table.php
+++ database/migrations/2019_06_20_191217_create_settings_table.php
@@ -14,7 +14,8 @@ class CreateSettingsTable extends Migration
     public function up()
     {
         Schema::create('settings', function (Blueprint $table) {
-            $table->string('key')->primary();
+            $table->bigIncrements('id');
+            $table->string('key')->unique();
             $table->string('value');
             $table->timestamps();
         });
